937ee11d2d76a2b13ddf54c2d7d55669a1be17de
erik <yokota082003@gmail.com>
2023-08-30 20:05:41 -0300

    Adding a post method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dtos/createUser.dto';
 
 @Controller('user')
 export class UserController {
   @Get()
   async GetAllUsers() {
     return JSON.stringify({ test: 'abc' });
+  }
+  @Post()
+  async CreateUsers(@Body() createUser: CreateUserDTO) {
+    return {
+      ...createUser,
+      password: undefined,
+    };
   }
 }

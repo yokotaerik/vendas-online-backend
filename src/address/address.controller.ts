772579e54f1ced542e78914185c1106edbf78f5e
erik <yokota082003@gmail.com>
2023-09-02 18:30:59 -0300

    adding adress service and controller

---
+++ src/address/address.controller.ts
@@ -0,0 +1,25 @@
+import {
+  Body,
+  Controller,
+  Param,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AddressService } from './address.service';
+import { CreateAddressDto } from './dtos/CreateAddress.dto';
+import { AddressEntity } from './entities/adress.entity';
+
+@Controller('address')
+export class AddressController {
+  constructor(private readonly adressService: AddressService) {}
+
+  @Post('/:userId')
+  @UsePipes(ValidationPipe)
+  async createAdress(
+    @Body() createAdressDTO: CreateAddressDto,
+    @Param('userId') userId: number,
+  ): Promise<AddressEntity> {
+    return await this.adressService.createAddress(createAdressDTO, userId);
+  }
+}
